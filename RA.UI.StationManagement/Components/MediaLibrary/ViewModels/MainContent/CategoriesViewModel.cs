f1d39acd2f0a785ed2dfc8bef6bf8d6674eeb37c
andreiv1 <vasilievici.andrei01@gmail.com>
2023-05-01 05:30:22 +0300

    .

--- RA.UI.StationManagement/Components/MediaLibrary/ViewModels/MainContent/CategoriesViewModel.cs
+++ RA.UI.StationManagement/Components/MediaLibrary/ViewModels/MainContent/CategoriesViewModel.cs
@@ -1,6 +1,9 @@
-﻿using RA.UI.Core.ViewModels;
+﻿using RA.DAL;
+using RA.DTO;
+using RA.UI.Core.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +12,22 @@ namespace RA.UI.StationManagement.Components.MediaLibrary.ViewModels.MainContent
 {
     public partial class CategoriesViewModel : ViewModelBase
     {
+        private readonly ICategoriesService categoriesService;
+
+        public ObservableCollection<CategoryDto> Categories { get; set; } = new();
+
+        public CategoriesViewModel(ICategoriesService categoriesService)
+        {
+            this.categoriesService = categoriesService;
+            _ = LoadCategories();
+        }
+
+        private async Task LoadCategories()
+        {
+            var categories = await categoriesService.GetRootCategoriesAsync();
+            foreach(var category in categories) { 
+                Categories.Add(category);
+            }
+        }
     }
 }
